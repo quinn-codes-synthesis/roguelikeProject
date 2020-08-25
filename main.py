e8900c59a59a713a840243bc770da39b536e9853
Quinn <wilkinsq@oregonstate.edu>
2020-08-25 19:54:55 -0400

    Create orc and troll entities
    Part 5 complete up to "The first step towards making our monsters scarier..."

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import copy
+
 import tcod
 
 from engine import Engine
-from entity import Entity
+import entity_factories
 from input_handlers import EventHandler
 from procgen import generate_dungeon
 
@@ -17,6 +19,8 @@ def main() -> None:
     room_min_size = 6
     max_rooms = 30
 
+    max_monsters_per_room = 2
+
     # tells tcod which font to use
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -25,7 +29,7 @@ def main() -> None:
     # event_handler is an instance of our EventHandler class
     event_handler = EventHandler()
 
-    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    player = copy.deepcopy(entity_factories.player)
 
     game_map = generate_dungeon(
         max_rooms=max_rooms,
@@ -33,6 +37,7 @@ def main() -> None:
         room_max_size=room_max_size,
         map_width=map_width,
         map_height=map_height,
+        max_monsters_per_room=max_monsters_per_room,
         player=player
     )
 
